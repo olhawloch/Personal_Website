a3a1a0b56ca06196e07c6a45bed5fcc71683ab4a
Olha Wloch <olha.wloch@gmail.com>
2022-01-03 14:21:14 -0500

    Added resume and bot

--- myapp.py
+++ myapp.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/robobert/')
+def robobert():
+    return render_template('bot.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
